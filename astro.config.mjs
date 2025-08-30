ea1728151a0b8bcd935e52045b1950fb785282e5
YafteDevCpp <yafte@ciencias.unam.mx>
2025-08-30 00:37:46 -0600

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://YafteDevCpp.github.io",
+  site: "https://yaftedevcpp.github.io",
   integrations: [tailwind(), icon(), compress()],
   output: "server",
   adapter: vercel()
