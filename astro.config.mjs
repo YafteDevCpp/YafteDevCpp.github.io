b1d42e48343eae09d6e06fc86853cf3708d2fd6b
yaftecpp <62233181+YafteDevCpp@users.noreply.github.com>
2025-08-29 23:09:20 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://YafteDevCpp.github.io"
+  site: "https://YafteDevCpp.github.io",
   integrations: [tailwind(), icon(), compress()],
   output: "server",
   adapter: vercel()
